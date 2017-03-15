8791aa3f5a755ce0e51dab814dc556193d1eb8d6
Chknbone <cb@palarran.com>
2017-03-16 11:10:57 +1200

    Add funcionality for map type buttons
    The three buttons at the top of the map app now work and you can choose
    which type of map you want to see (Regular, Satellite, or Hybrid).

--- app/src/main/java/com/palarran/mapone/MainActivity.java
+++ app/src/main/java/com/palarran/mapone/MainActivity.java
@@ -2,6 +2,7 @@ package com.palarran.mapone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,8 +11,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     //Defining Google Map objects variables
     GoogleMap mMap;
     boolean mapReady=false;
@@ -26,11 +27,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
-    //Google map object that will change the map fragment in the .xml
+    //Hooking up the Normal map view button.
+    public void mapRegular(View v) {
+        if (mapReady) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    //Hooking up the Satellite map view button.
+    public void mapSatellite(View v) {
+        if (mapReady) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+    }
+
+    //Hooking up the Hybrid map view button.
+    public void mapHybrid(View v) {
+        if (mapReady) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+    }
+
+    //Google map object that will change the map fragment in activity_main.xml
     @Override
-    //onMapReady CallBack
+    //onMapReady CallBack method
     public void onMapReady(GoogleMap map) {
 
+        //Setting mapReady to true
         mapReady=true;
         //Loading local instance map from Callback
         mMap = map;
@@ -38,12 +61,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //Creating a new LatLng. i.e. Location Map starts up at
         LatLng fiji = new LatLng(-17.7721, 177.1925);
 
-        //Setting camera position with builder and setting target at the LatLong and
-        //setting camera starting height(zoom level) as well
+        //Setting camera position with builder, setting target at the LatLong and setting camera
+        //starting height(zoom level) as well
         CameraPosition target = CameraPosition.builder().target(fiji).zoom(15).build();
 
-        //Setting position to the target
+        //Setting position to the target created above
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-
     }
 }
